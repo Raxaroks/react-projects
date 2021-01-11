496c9524a8f9278a49eec95bfb1b59f726d02006
= <=>
2021-01-11 14:04:06 -0600

    Section 10 finished: useReducer

---
+++ 05-hooks-app/src/components/08-useReducer/TodoList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import TodoListItem from './TodoListItem';
+// import PropTypes from 'prop-types';
+
+const TodoList = ( {todos, handleDelete, handleToggle} ) => {
+
+    return (
+        <ul className='list-group list-group-flush'>
+            {
+                todos.map( (todo, i) => (
+                    <TodoListItem
+                        key = {todo.id}
+                        todo = {todo}
+                        index = {i}
+                        handleDelete = {handleDelete}
+                        handleToggle = {handleToggle}
+                    />
+                ) )
+            }
+        </ul>
+    );
+
+
+}
+
+export default TodoList;
--- 05-hooks-app/src/index.js
+++ 05-hooks-app/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Padre from './components/07-tarea-memo/Padre';
+import TodoApp from './components/08-useReducer/TodoApp';
+
+// import Padre from './components/07-tarea-memo/Padre';
 // import CallbackHook from './components/06-memos/CallbackHook';
 // import MemoHook from  './components/06-memos/MemoHook';
 // import Memorize from './components/06-memos/Memorize';
@@ -15,7 +17,7 @@ import Padre from './components/07-tarea-memo/Padre';
 // import HookApp from './HookApp'; 
 
 ReactDOM.render(
-    <Padre />, // componente raíz
+    <TodoApp />, // componente raíz
     document.getElementById('root')
 ); 
 
